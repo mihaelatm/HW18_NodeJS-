2b62e33279a95ef7c25046182d49517d7dac33fe
Talmazan Mihaela <talmazanmihaela@gmail.com>
2024-12-05 16:16:06 +0100

    Added Task 2

--- index.ts
+++ index.ts
@@ -17,3 +17,47 @@ const adminUser: AdminUser = {
   permissions: ["read", "write", "delete"],
   email: "alice.smith@example.com",
 };
+
+console.log(adminUser);
+
+///==============Task2=======================
+
+interface Engine {
+  type: string;
+  horsepower: number;
+}
+
+interface Car {
+  make: string;
+  model: string;
+  engine: Engine;
+  year?: number;
+}
+
+function carInfo(car: Car): void {
+  console.log(
+    `Make: ${car.make},
+        Model: ${car.model},
+        Engine Type: ${car.engine.type},
+        Horsepower: ${car.engine.horsepower}, 
+        `
+  );
+
+  if (car.year) {
+    console.log(`Year: $car.year`);
+  } else {
+    console.log(`Year: Not provided`);
+  }
+}
+
+const myCar: Car = {
+  make: "Toyota",
+  model: "Camry",
+  engine: {
+    type: "Gasoline",
+    horsepower: 200,
+  },
+  year: 2022,
+};
+
+carInfo(myCar);
